385ed9c55e3465201da375f11b2a5a75e7031852
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-08-05 01:18:41 +0500

    Add Responsive Layout, Navbar, Hamburger Menu Design, Hero Section and Search bar

---
+++ client/pages/HomePage/HomePage.jsx
@@ -0,0 +1,41 @@
+import SearchBar from "../../components/Searchbar/Searchbar";
+import "./HomePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Discover the perfect property with ease. Whether you're buying,
+            renting, or investing, our platform connects you with verified
+            listings, trusted agents, and powerful tools to help you make
+            confident decisions. Browse homes, explore neighborhoods, and take
+            virtual tours—all in one place!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar/Navbar";
+import HomePage from "../pages/HomePage/HomePage";
+import "./layout.scss";
+
+function App() {
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
