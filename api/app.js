ea8277fca678c6f7f8f2db408862e320cbef071f
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-08-29 03:10:32 +0500

    Add express routes

---
+++ api/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running!!");
+});
