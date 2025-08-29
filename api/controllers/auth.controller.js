4f604f9c4cf6e52baa0148bc4db21325d9ec2641
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-08-30 02:59:50 +0500

    feat: setup prisma with mongodb support

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(hashedPassword);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
