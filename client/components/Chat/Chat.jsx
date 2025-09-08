4e44cb41a210a57f90bf8c2b024c25135a9b7990
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-09-08 23:43:26 +0500

    feat: implemented real-time message passing using socket functions with DB persistance

--- client/components/Chat/Chat.jsx
+++ client/components/Chat/Chat.jsx
@@ -2,11 +2,14 @@ import { useState, useContext } from "react";
 import "./Chat.scss";
 import { AuthContext } from "../../src/context/AuthContext";
 import apiRequest from "../../src/lib/apiRequest";
+import { SocketContext } from "../../src/context/SocketContext";
+
 import { format } from "timeago.js";
 
 function Chat({ chats }) {
   const [chat, setChat] = useState(null);
   const { currentUser } = useContext(AuthContext);
+  const { socket } = useContext(SocketContext);
 
   const handleOpenChat = async (id, receiver) => {
     try {
@@ -31,10 +34,36 @@ function Chat({ chats }) {
       const res = await apiRequest.post("/messages/" + chat.id, { text });
       setChat((prev) => ({ ...prev, messages: [...prev.messages, res.data] }));
       e.target.reset();
+      socket.emit("sendMessage", {
+        receiverId: chat.receiver.id,
+        data: res.data,
+      });
     } catch (err) {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    const read = async () => {
+      try {
+        await apiRequest.put("/chats/read/" + chat.id);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (chat && socket) {
+      socket.on("getMessage", (data) => {
+        if (chat.id === data.chatId) {
+          setChat((prev) => ({ ...prev, messages: [...prev.messages, data] }));
+          read();
+        }
+      });
+    }
+    return () => {
+      socket.off("getMessage");
+    };
+  }, [socket, chat]);
 
   return (
     <div className="chat">
